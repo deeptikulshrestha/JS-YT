67dd4345de41d41ae149819de2333fe059ccfacf
DEEPTI KULSHRESTHA <110666921+deeptikulshrestha@users.noreply.github.com>
2024-08-31 06:37:39 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+//console.log(2==2);
+//console.log(2>2);
+
+console.log("2" > 1); //true but not recommended use same datatype
+console.log(null ==0); //not recommend
+console.log(null >= 0);
+console.log(null < 0);
+
+// === strict check
+
+console.log("2" === 2); //false
+
+
+
+
+
