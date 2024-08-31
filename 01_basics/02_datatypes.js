0ce645b412b40c74dc7879833cc874bd358c03b2
DEEPTI KULSHRESTHA <110666921+deeptikulshrestha@users.noreply.github.com>
2024-08-31 06:01:37 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as new version
+
+//alert(3+3) // we are using node.js not browser
+
+let name = "deepti"
+let age = 20
+let isLoggedIn = true
+
+//number 2 to power 53
+//bigint
+//string ""
+//boolean
+//null => standalone value
+//undefined =>not assigned
+//symbol => for unique
+
+
+//object 
+
+console.log(typeof null); //object
+
