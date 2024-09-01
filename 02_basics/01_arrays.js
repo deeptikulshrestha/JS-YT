0182202b511ff4a86061466309f6be4b9f6b460e
DEEPTI KULSHRESTHA <110666921+deeptikulshrestha@users.noreply.github.com>
2024-09-01 06:05:32 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,28 @@
+const arr = [1,2,32,12,3,true,"deepti"]
+const myHeros = ["shakti", "batman", "krish"]
+const myArr = new Array(2,7,9,3,5,0)
+
+console.log(arr[0]);
+
+//Methods
+
+console.log(myArr.push(10));
+console.log(myArr);
+
+myArr.unshift(6)
+myArr.shift()
+console.log(myArr);
+
+const newArr = myArr.join()
+console.log("A" ,newArr);
+
+
+//slice =>  does not manipulate the original array
+//splice => manipulates the original array
+
+
+
+
+
+
+
