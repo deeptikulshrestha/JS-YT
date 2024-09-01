e98ea49a3b154f32daa0fa4d18fb057714a51f51
DEEPTI KULSHRESTHA <110666921+deeptikulshrestha@users.noreply.github.com>
2024-09-01 04:57:06 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,35 @@
+const name = "deepti"
+const repoCount = 5
+console.log(name + repoCount + "Value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("PUBG-BGMI-VALO")
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2));
+console.log(gameName);
+
+const newString = gameName.substring(0,3)
+console.log(newString);
+
+const anotherString = gameName.slice(-3,3)
+console.log(anotherString);
+
+
+const newStr = "                  Deepti                    "
+console.log(newStr);
+
+console.log(newStr.trim());
+
+
+const url = "https://hdguhujggd/deeptikuls"
+console.log(url.replace('hdgu', 'http'))
+
+
+console.log(gameName.split('-'));
+
+
+
+
+
