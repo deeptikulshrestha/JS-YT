2eb77554a5f0d15eca1548250c261f62ad2ee4d8
DEEPTI KULSHRESTHA <110666921+deeptikulshrestha@users.noreply.github.com>
2024-09-01 05:19:35 +0000

    Maths and Numbers

---
+++ 01_basics/06_maths_num.js
@@ -0,0 +1,48 @@
+const score = 300
+console.log(score);
+
+
+const balance = new Number(4000)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toFixed(1));
+
+const preNum = 23.45667
+console.log(preNum.toPrecision(3));
+
+
+const hundreds =  10000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+//****************Maths*********************
+
+console.log(Math);
+console.log(Math.abs(-4));//converts to positive
+console.log(Math.round(9.6));
+console.log(Math.ceil(8.2));
+console.log(Math.floor(9.6));
+console.log(Math.min(3,4,1,33));
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1))+min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
