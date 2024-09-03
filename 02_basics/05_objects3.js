87754105d64feafdea07f9993cbe32bc5a504fca
DEEPTI KULSHRESTHA <110666921+deeptikulshrestha@users.noreply.github.com>
2024-09-03 05:10:13 +0000

    Json

---
+++ 02_basics/05_objects3.js
@@ -0,0 +1,29 @@
+const course = {
+    name: "js in hindi",
+    price: "999",
+    courseInst : "Hitesh"
+}
+
+//console.log(course.courseInst);
+//object destructure
+const {courseInst:instructor} = course
+console.log(instructor);
+
+//json
+/*{
+    "name":"deepti",
+    "coursename" : "js in hindi",
+    "price1" : "0"
+}*/
+
+//or
+
+/*
+[
+{
+},
+{},
+] */
+
+
+
