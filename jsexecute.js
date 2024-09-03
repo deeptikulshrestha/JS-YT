6388c7637b9d7455c148845920e379f913aee233
DEEPTI KULSHRESTHA <110666921+deeptikulshrestha@users.noreply.github.com>
2024-09-03 07:22:29 +0000

    Js Execution

---
+++ jsexecute.js
@@ -0,0 +1,10 @@
+//How JAVASCRIPT runs code?
+
+//JAVASCRIPT EXECECUTION CONTEXT <= THIS
+//1. GLOBAL EXEUTION CONTEXT
+//2. FUNCTIONAL EXECUTION CONTEXT
+//3. EVAL EXECUTION CONTEXT
+
+//{ } =>
+    //1. MEMORY CREATION PHASE => ALL Variables are assigned undefined but function is assinged func defition
+    //2. EXECUTION PHASE => given values are assigned to variables, function creates one more new execution context annd will get deleted after function is executed
