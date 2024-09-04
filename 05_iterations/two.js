205b845ab7a440cd476520caba0ad746a9ba8b14
DEEPTI KULSHRESTHA <110666921+deeptikulshrestha@users.noreply.github.com>
2024-09-04 08:51:21 +0000

    While do While

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+
+let i = 1;
+while(i<10){
+    console.log(i);
+    i++;
+    
+}
+
+const myArr = [1,4,5,6,3,"deepti"]
+let arr = 0;
+while(arr<myArr.length){
+    console.log(`Value is ${myArr[arr]}`);
+    arr++;
+    
+}
+
+let score = 1;
+do{
+   console.log(`Score is ${score}`);
+   score++;
+    
+}while(score<10);
