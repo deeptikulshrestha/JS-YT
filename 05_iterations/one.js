b902f35570f6061eb62d8d2163195557709ac456
DEEPTI KULSHRESTHA <110666921+deeptikulshrestha@users.noreply.github.com>
2024-09-04 08:44:24 +0000

    Iterations

---
+++ 05_iterations/one.js
@@ -0,0 +1,22 @@
+// for
+for ( let i = 0 ; i<=10;i++){
+    const element = i;
+    console.log(element);
+    
+}
+
+//break 
+for(let i = 1 ; i<=10 ; i++){
+    console.log(i);
+    if(i==7) break;
+    
+}
+
+//continue
+
+for(let i = 1 ; i<20;i++){
+    if(i==15) continue;
+    console.log(i);
+    
+    
+}
