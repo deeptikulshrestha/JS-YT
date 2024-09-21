00727b1dfaf90a8bb0a290a76cca9786d212b71c
DEEPTI KULSHRESTHA <110666921+deeptikulshrestha@users.noreply.github.com>
2024-09-21 05:06:47 +0000

    Unlimited Colors

---
+++ UNLIMITED COLORS/one.js
@@ -0,0 +1,22 @@
+//generate random colorss
+const randColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = "#"
+    for(let i = 0;i<6;i++){
+        color += hex[Math.floor(Math.random()*16)]
+    }
+    return color 
+}
+let intervalId
+const startChange = function(){
+    if(!intervalId){
+    intervalId = setInterval(changeBgColor,1000)}
+    function changeBgColor(){
+    document.body.style.backgroundColor = randColor();
+}}
+const stopChange = function(){
+    clearInterval(intervalId)
+    intervalId = null;
+}
+document.querySelector('#start').addEventListener('click',startChange)
+document.querySelector('#stop').addEventListener('click',stopChange)
